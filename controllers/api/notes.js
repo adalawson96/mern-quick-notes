9721c8974cc582335edd916807df8ebe5b2a2454
adalawson96 <adalawson96@gmail.com>
2023-04-06 21:45:16 +0200

    create a note

---
+++ controllers/api/notes.js
@@ -0,0 +1,20 @@
+// const jwt = require('jsonwebtoken');
+// const User = require('../../models/user');
+// const bcrypt = require('bcrypt');
+const Note = require('../../models/note');
+
+module.exports = {
+  create,
+};
+
+async function create(req, res) {
+  try {
+    req.body.user = req.user._id 
+    
+    const createdNote = await Note.create(req.body);
+    
+    res.json(createdNote)
+  } catch (err) {
+    res.status(400).json(err)
+  }
+}
